b6b07a15211035ce03085975921af71cce758d5b
noor20-meet <noor20@meet.mit.edu>
2018-10-21 19:26:01 +0300

    whatever

--- lab2.py
+++ lab2.py
@@ -6,14 +6,20 @@ b=[1,2,3]
 y=newlist(b)
 print(y)
 '''
+
 list1= [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-'''list2= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]'''
+list2=[]
+for number in list1:
+	if number < 5:
+		print(number)
+		list2.append(number)
+print(list2)
+
 user_option=input("enter a number")
 user_option_list=[]
-
-
-
-
-
+for number1 in list1:
+	if number1<int(user_option):
+		user_option_list.append(number1)
+print (user_option_list)
 
 
--- lab3.py
+++ lab3.py
@@ -1,37 +1,37 @@
 import turtle
-'''
-#q1
-turtle.pensize(10)
-turtle.goto(-100,0)
-turtle.pencolor("red")
-turtle.right(0)
-turtle.forward(200)
-turtle.pencolor("blue")
-turtle.right(150)
-turtle.pencolor("green")
-turtle.forward(200)
-turtle.pencolor("yellow")
-turtle.right(140)
-turtle.pencolor("pink")
-turtle.forward(200)
-turtle.right(140)
-turtle.pencolor("indigo")
-turtle.forward(200)
-turtle.pencolor("grey")
-turtle.goto(-100,0)
-'''
 
+#q1
+angle=150
+length=150
+turtle.pensize(10)
+for i in range(5):
+	turtle.forward(length)
+	turtle.right(angle)
+
+'''
 #q2
 
 import turtle
+turtle.begin_fill()
 turtle.forward(10)
 turtle.right(90)
 turtle.forward(10)
 turtle.right(30)
-turtle.forward(10)
+turtle.forward(50)
+turtle.right(120)
+turtle.forward(50)
 turtle.right(30)
-turtle.forward(10)
-turtle.right(90)
+turtle.forward(50)
+turtle.end_fill()
 
 turtle.mainloop()
 
+'''
+short_forward=20
+backward=10
+
+for d in range(100):
+	turtle.penup()
+	turtle.home()
+	turtle.pendown()
+	turtle.forward()
