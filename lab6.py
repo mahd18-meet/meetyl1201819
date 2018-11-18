a5ae3a40ecff6c841b8c29abb76fdcebbc9f10c2
noor20-meet <noor20@meet.mit.edu>
2018-11-18 19:05:21 +0200

    lab 6

--- lab6.py
+++ lab6.py
@@ -1,8 +1,45 @@
-import Turtle
+
+import random
+import turtle
+from turtle import *
+colormode(255)
+'''
 class Square(Turtle):
 	def __init__(self, size):
-		square.shape("square")
-		square.shapesize(size)
+		Turtle.__init__(self)
+		self.shape("square")
+		self.shapesize(size)
 
 	def random_color(self):
-		
+		rgb = (random.randint(0,256),random.randint(0,256),random.randint(0,256))
+		self.fillcolor(rgb)
+
+Judeh = Square(10)
+Judeh.random_color()
+'''
+'''
+class Rectangle(Turtle):
+	def __init__(self,width, height):
+		Turtle.__init__(self)
+		self.width = width
+		self.height  = height
+		turtle.register_shape("rectangle",((50,0),(50,30),(0,30),(0,0)))
+		self.shape("rectangle")
+
+	def random_color(self):
+		rgb = (random.randint(0,256),random.randint(0,256),random.randint(0,256))
+		self.fillcolor(rgb)
+
+Jimmy = Rectangle(10,20)
+Jimmy.random_color()
+'''
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
