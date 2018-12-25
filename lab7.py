febfc69c8702021ecef1051a735eadc51fd12b0f
noor20-meet <noor20@meet.mit.edu>
2018-12-25 19:08:59 +0200

    lab7

--- lab7.py
+++ lab7.py
@@ -2,46 +2,65 @@ from turtle import *
 import random
 import math
 import turtle
+turtle.colormode(255)
+
+width=300
+height=300
 
 class Ball(Turtle):
-	def __init__(self,radius,color,speed):
+	def __init__(self,radius,color,dx,dy):
 		Turtle.__init__(self)
 		self.shape("circle")
 		self.shapesize(radius/10)
 		self.radius=radius
 		self.color(color)
-		self.speed(speed)
+		self.dx=dx
+		self.dy=dy
+	def move(self):
+		oldx=self.xcor()
+		oldy=self.ycor()
+		newx=oldx+self.dx
+		newy=oldy+self.dy
+		self.goto(newx,newy)
+		if (newx>width) or (newx<-width):
+			self.dx = -self.dx
+		if newy>height or newy<-height:
+			self.dy=-self.dy
+		
 
-ball1=Ball(50,"green",2)
-ball2=Ball(40,"blue",5)
-ball3=Ball(20,"yellow",4)
-ball4=Ball(10,"red",7)
-balls_list=[ball1,ball2,ball3,ball4]
 
-ball1.penup()
-ball2.penup()
-ball1.goto(0,0)
-ball2.goto(0,90)
 
-def check_collision(ball1,ball2):
-	x1=ball1.xcor()
-	x2=ball2.xcor()
-	y1=ball1.ycor()
-	y2=ball2.ycor()
-	r1=ball1.radius
-	r2=ball2.radius
+
+ball1=Ball(50,"green",2,3)
+ball2=Ball(40,"blue",5,7)
+
+def check_collision(balla,ballb):
+	x1=balla.xcor()
+	x2=ballb.xcor()
+	y1=balla.ycor()
+	y2=ballb.ycor()
+	r1=balla.radius
+	r2=ballb.radius
 	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
 	if r1+r2<D:
 		print("The balls did not collide")
 	elif r1+r2>D or r1+r2==D:
-		ball1.color("red")
-		ball2.color("red")
+		rgb = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+		balla.color(rgb)
+		ballb.color(rgb)
+		if balla.radius>ballb.radius:
+			smaller=ballb
+		else:
+			smaller=balla
+		smaller.goto(random.randint(-300,300),random.randint(-300,300))
 
-f=check_collision(ball1,ball2)
-
-	
-
-
+while True:
+	ball1.penup()
+	ball2.penup()
+	ball1.move()
+	ball2.move()
+	ballslist=[ball1,ball2]
+	check_collision(ball1,ball2)
 
 
 
