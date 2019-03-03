f413064f3bee85abf843b502968182e8f360aac4
noor20-meet <noor20@meet.mit.edu>
2019-03-03 17:26:41 +0200

    hi

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
+#secondary file for classes and functions
 from turtle import *
 import turtle
 
+
+
+#making the food that spawns on the right of the screen
 class Food(Turtle):
 	def __init__(self, x, y, r):
 		Turtle.__init__(self)
@@ -8,20 +12,23 @@ class Food(Turtle):
 		self.r = r
 		self.hideturtle()
 		self.goto(x,y)
+	def move_food(self):
+		self.backward(.1)
 
+
+
+#hiding annoying random turtle dont worry about this line
 turtle.hideturtle()
 
+#photos for the food
 turtle.register_shape("burger1.gif")
 turtle.register_shape("pizzaf.gif")
 turtle.register_shape("sushi.gif")
-turtle.register_shape("trash.gif")
 
-trash=Food(730,-400,30)
-trash.penup()
-trash.shape("trash.gif")
-trash.showturtle()
 
+#making the Burger, Pizza, and Sushi
 burger = Food (450, 290, 50)
+burger.shape("circle")
 burger.shape("burger1.gif")
 turtle.penup()
 pizza = Food (570, 130, 50)
@@ -30,15 +37,16 @@ sushi = Food (650, -50, 50)
 sushi.shape("sushi.gif")
 turtle.tracer(1)
 
+
+#Making Abed's class cus he's very special
 class Player(Turtle):
 	def __init__(self,r):
 		Turtle.__init__(self)
 		self.penup()
 		self.goto(150,0)
-
 		self.r=r
 
-
+#making the class for the tables 
 class Table(Turtle):
 	def __init__(self, x,y,r):
 		Turtle.__init__(self)
--- nunu.py
+++ nunu.py
@@ -1,3 +1,4 @@
+#main folder, where all the game is
 from turtle import *
 import turtle
 import time 
@@ -7,22 +8,56 @@ from functions import *
 from tkinter import *
 from characters import *
 
-turtle.tracer(100,1)
-turtle.register_shape("abed.gif")
-turtle.register_shape("gameover.gif")
 
+#higher tracer = faster characters spawning from the left
+turtle.tracer(125,1)
+
+#main character is Abed
+turtle.register_shape("abed.gif")
 abed = Player (20)
-abed.shape("abed.gif")
+abed.shape("circle")
 abed.shapesize(1)
 abed.penup()
-turtle.bgpic("tapperbg.gif")
+	
 
-RUNNING = True
+#life and score counters
+Life = 3
+
+#life hearts
+turtle.register_shape("hearts0.gif")
+turtle.register_shape("hearts1.gif")
+turtle.register_shape("hearts2.gif")
+turtle.register_shape("hearts3.gif")
+hearts=turtle.Turtle()
+hearts.penup()
+hearts.shape("hearts3.gif")
+hearts.goto(-700, 450)
+
+#score counter
+score_count = 0
+score=turtle.Turtle()
+score.penup()
+score.hideturtle()
+score.goto(200,450)
+score.write(str("SCORE: " + str(score_count)),move=True,align = "center",font=("CHARLESWORTH",25,"bold"))
+
+
+#game over 
+turtle.register_shape("gameover.gif")
+gameover=turtle.Turtle()
+gameover.shape("gameover.gif")
+gameover.hideturtle()
+
+
+#controls for the player
 UP = 0
 DOWN = 1
 LEFT = 2
 RIGHT = 3
 direction = UP
+
+
+#creating the tables to place food on
 foods=[burger,pizza,sushi]
 FOODCOLLISION= False
 table=Food(200,280,40)
@@ -30,33 +65,35 @@ table2=Food(200,70,40)
 table3=Food(200,-150,40)
 table4=Food(200,-360,40)
 tables = [table,table2,table3,table4]
-number = 0
 
+
+#controls
 def up():
 	global direction
 	direction=UP 
-	abed.goto(abed.xcor(), abed.ycor() + 20)
-
+	abed.goto(abed.xcor(), abed.ycor() + 55)#change this value to change the speed at Abed moves
 def down():
     global direction
     direction = DOWN
-    abed.goto(abed.xcor(), abed.ycor() - 20)
+    abed.goto(abed.xcor(), abed.ycor() - 55)#change this value to change the speed at Abed moves
 
 def left():
     global direction
     direction = LEFT
-    abed.goto(abed.xcor() - 20, abed.ycor())
+    abed.goto(abed.xcor() - 55, abed.ycor())#change this value to change the speed at Abed moves
 
 def right():
     global direction
     direction = RIGHT
-    abed.goto(abed.xcor() + 20, abed.ycor())
+    abed.goto(abed.xcor() + 55, abed.ycor())#change this value to change the speed at Abed moves
 
 turtle.onkeypress(up, "Up")
 turtle.onkeypress(down, "Down")
 turtle.onkeypress(left, "Left")
 turtle.onkeypress(right, "Right")
 
+
+#making the collide function, essential for the rest of the game
 def collide(ball_a,ball_b):
 	x1=ball_a.xcor()
 	x2=ball_b.xcor()
@@ -68,130 +105,153 @@ def collide(ball_a,ball_b):
 	if D < ball_a.r + ball_b.r:
 		return True
 
+
+
+#checking if Abed has collided with the food on the right 
 def check_food_collision():
 	for f in foods:
 		if collide(abed,f) == True:
+			#showing the food whenever Abed touches it
 			f.showturtle()
+			#making the food follow abed whenever he picks them up
 			f.goto(abed.xcor(),abed.ycor())
+			
 				
 	return True
 
+
+#checking if the food has collided with the tables and making sure they are alligned for desire_food_player collsion
 def slide_food():
+	global number
 	for f in foods:
 		for t in tables:
 			if collide(f,t) == True:
+				#making the food go to the begining of the table
 				f.goto(t.xcor(),t.ycor())
-				f.bk(200)
+				#making the food move a bit to the left whenever it touches the table
+				f.bk(50)
 
-def check_player_food_collision():
-	players = getplayers()
-	for f in foods:
-		for player in players:
-			if collide(f,player) == True:
-				f.hideturtle()
-				p = random.randint(0,3)
-				a = random.randint(0,4)
-				player.goto(starting_positions[p])
-				player.shape(shapes[a])
-				player.setheading(0)
-				if f == burger:
-					f.goto(450, 290)
-				elif f == pizza:
-					f.goto(570, 130)
-				elif f == sushi:
-					f.goto(650, -50)			
 	return True
 
-def disposing():
-	for f in foods:
-		if collide(f,trash) == True:
-			f.hideturtle()
-			if f == burger:
-				f.goto(450, 290)
-			elif f == pizza:
-				f.goto(570, 130)
-			elif f == sushi:
-				f.goto(650, -50)
 
-alert=turtle.Turtle()
-alert.color("red")
-alert.hideturtle()
-alert.penup()
-alert.goto(-500,400)
-
-tyabed=turtle.Turtle()
-tyabed.penup()
-tyabed.hideturtle()
-tyabed.color("red")
-tyabed.goto(400,-400)
-
-cross = turtle.Turtle()
-cross.hideturtle()
-cross.penup()
-cross.goto(140,265)
-cross.pensize(5)
-cross.pencolor("red")
-
-# cross.goto(330,360)
-# cross.pendown()
-# cross.goto(140,265)
-# cross.penup()
-# cross.goto(140,360)
-# cross.pendown()
-# cross.goto(330,265)
-
-def attack():
-	global RUNNING
-	players = getplayers()
-	for p in players:
-		for t in tables:
-			if collide(p,t) == True:
-				p.setheading(p.towards(abed))
-				p.forward(0.01)
-				if collide(p,abed) == True:
-					# for i in range(3):
-					# 	alert.write("YOU HAVE BEEN ATTACKED BY A HUNGRY STUDENT", True, font=("CHARLESWORTH",20,"bold"))
-					# 	alert.clear()
-					# 	time.sleep(1)
-					# 	alert.goto(-500,400)
-					RUNNING = False
+#biggest function, checking the collision between customer requests, the customers, and the food
+def check_food_desire_player_collision():
+	global Life,score_count
+	for a in foods:
+		for b in desires:
+			for c in players:
+				if collide(a,c) == True and collide(a,b):
+					#checking if the food given to the customer is the correct one
+					if a.shape() == b.shape():
+						#hiding the food giving the impression a customer is "satisfied"
+						a.hideturtle()
+						#increases score
+						score_count += 25
+						score.clear()
+						score.goto(200,450)
+						score.write(str("SCORE: " + str(score_count)),move=True,align = "center",font=("CHARLESWORTH",25,"bold"))
+						#random value to decide new picture for customers
+						e = random.randint(0,4)
+						c.shape(shapes[e])
+						#random value to decide the new position the customer will go to
+						p = random.randint(0,3)
+						c.goto(starting_positions[p])
+						#random value to decide new customer request
+						g = random.randint(0,2)
+						b.shape(food_choices[g])
+						#making the request of the customer follow the customer
+						b.goto(c.xcor()+90, c.ycor()-50)
+						#after the food collides, it goes back to its starting positions on the right
+						if a == pizza:
+							a.goto(570,130)
+						elif a == sushi:
+							a.goto(650,-50)
+						else:
+							a.goto(450,290)
 
 
+					else:
+						#same as top except we subtract a point from life
+						Life -= 1
+						a.hideturtle()
+						e = random.randint(0,4)
+						c.shape(shapes[e])
+						p = random.randint(0,3)
+						c.goto(starting_positions[p])
+						g = random.randint(0,2)
+						b.shape(food_choices[g])
+						b.goto(c.xcor()+90, c.ycor()-50)
+						if a == pizza:
+							a.goto(570,130)
+						elif a == sushi:
+							a.goto(650,-50)
+						elif a == burger:
+							a.goto(450,290)
+						if Life == 3:
+							hearts.shape("hearts3.gif")
+						elif Life == 2:
+							hearts.shape("hearts2.gif")
+						elif Life == 1:
+							hearts.shape("hearts1.gif")
+
+
+
+#this funcion checks if the customers have reached too far on the table and they sacrifice a Life
+def check_if_player_reached_table():
+	global Life
+	for a in desires:
+		for b in players:
+			if a.xcor() > 200 and b.xcor() > 150:
+				Life -= 1
+				e = random.randint(0,4)
+				b.shape(shapes[e])
+				p = random.randint(0,3)
+				b.goto(starting_positions[p])
+				g = random.randint(0,2)
+				a.shape(food_choices[g])
+				a.goto(b.xcor()+90, b.ycor()-50)
+				if Life == 3:
+					hearts.shape("hearts3.gif")
+				elif Life == 2:
+					hearts.shape("hearts2.gif")
+				elif Life == 1:
+					hearts.shape("hearts1.gif")
+
+
+#listen so the keys can control Abed		
 turtle.listen()
+
 frame = 0
 
-while RUNNING:	
+
+#main loop to make game move
+while Life > 0:	
 	check_food_collision()
 	frame+=1
-	if frame/1500 == 1:
+	#change the value in frame/x to control how often players spawn
+	if frame/2000 == 1:
 		frame = 0
 		spawn_player()
 	move_players()
-	check_player_food_collision()
+	move_desires()
+	check_food_desire_player_collision()
+	check_if_player_reached_table()
 	slide_food()
-	disposing()
-	attack()
 	turtle.update()
-
-if RUNNING == False:
+#this section plays whenever you lose
+while Life == 0:
+	hearts.shape("hearts0.gif")
+	gameover.showturtle()
 	abed.hideturtle()
-	# for player in players:
-	# 	player.hideturtle()
-
-	turtle.bgpic("gameover.gif")
-	cross.goto(330,360)
-	cross.pendown()
-	cross.goto(140,265)
-	cross.penup()
-	cross.goto(140,360)
-	cross.pendown()
-	cross.goto(330,265)
-	cross.penup()
+	for a in players:
+		a.hideturtle()
+	for d in desires:
+		d.hideturtle()
 
 
-	cross.goto(360,310)
-	cross.color("black")
-	cross.write("Abed", True, font = ("CHARLESWORTH",30,"bold"))
-	tyabed.write("P.S: thank you Abed <3", True, font = ("CHARLESWORTH",20,"bold"))
+
+
+
+
 
 turtle.mainloop()
